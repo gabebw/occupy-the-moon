ad7d439dae35a961f52b5e78a8471e4fea82cc0f
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-12-14 08:58:00 -0500

    Switch to SCSS and Bourbon.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 
 gem 'sinatra', '~> 1.3.1'
+gem 'sass', '~> 3.1'
+gem 'bourbon', '~> 1.2'
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
+require 'bundler/setup'
+require 'sass'
+require './views/css/bourbon/lib/bourbon'
 require 'sinatra'
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/css/:stylesheet.css' do
+  scss params[:stylesheet].to_sym, views: settings.views + '/css'
 end
